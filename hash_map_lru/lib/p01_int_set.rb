da2f93a26c52a26c04d15a0cddc3f54da68fcb58
Torben Ginsberg <tginsberg8@gmail.com>
2022-06-20 14:40:15 -0400

    resizing int

--- hash_map_lru/lib/p01_int_set.rb
+++ hash_map_lru/lib/p01_int_set.rb
@@ -1,23 +1,25 @@
+require "byebug"
+
 class MaxIntSet
   attr_reader :max, :store
 
   def initialize(max)
     @max = max
-    @store = Array.new(max + 1, false)
+    @store = Array.new(max, false)
   end
 
   def insert(num)
-    raise "Out of bounds" if !is_valid?(num)
+    validate!(num)
     @store[num] = true
   end
 
   def remove(num)
-    raise "Out of bounds" if !is_valid?(num)
+    validate!(num)
     @store[num] = false
   end
 
   def include?(num)
-    raise "Out of bounds" if !is_valid?(num)
+    validate!(num)
     @store[num] == true
   end
 
@@ -28,6 +30,7 @@ class MaxIntSet
   end
 
   def validate!(num)
+    raise "Out of bounds" if !is_valid?(num)
   end
 end
 
@@ -40,6 +43,7 @@ class IntSet
   end
 
   def insert(num)
+    return if include?(num)
     self[num] << num
   end
 
@@ -65,7 +69,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_accessor :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -73,18 +77,33 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num
+    @count += 1
+    resize! if num_buckets < self.count
+    # if !self[num].include?(num)
+    #   self[num] << num
+    #   @count += 1
+    # end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    bucket_idx = num % num_buckets
+    store[bucket_idx]
   end
 
   def num_buckets
@@ -92,5 +111,9 @@ class ResizingIntSet
   end
 
   def resize!
+    curr_store = self.store
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) { Array.new }
+    curr_store.flatten.each { |num| self.insert(num) }
   end
 end
