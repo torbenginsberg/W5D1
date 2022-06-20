def0ce36063d92deb0cca37d5c33037aac059516
Torben Ginsberg <tginsberg8@gmail.com>
2022-06-20 15:42:12 -0400

    hash set

--- hash_map_lru/lib/p02_hashing.rb
+++ hash_map_lru/lib/p02_hashing.rb
@@ -3,24 +3,32 @@ class Integer
 end
 
 class Array
+  # def hash
+  #   curr_xor = 0
+  #   self.each_with_index do |ele, idx|
+  #     temp = ele.object_id + idx
+  #     curr_xor = curr_xor ^ temp
+  #   end
+  #   curr_xor.hash
+  # end
   def hash
-    curr_xor = 0
-    self.each_with_index do |ele, idx|
-      temp = ele.object_id + idx
-      curr_xor = curr_xor ^ temp
+    each_with_index.inject(0) do |intermediate_hash, (el, i)|
+      (el.hash + i.hash) ^ intermediate_hash
     end
-    curr_xor.hash
   end
 end
 
 class String
+  # def hash
+  #   curr_xor = 0
+  #   chars.each_with_index do |char, idx|
+  #     temp = char.object_id + idx
+  #     curr_xor = curr_xor ^ temp
+  #   end
+  #   curr_xor.hash
+  # end
   def hash
-    curr_xor = 0
-    chars.each_with_index do |char, idx|
-      temp = char.object_id + idx
-      curr_xor = curr_xor ^ temp
-    end
-    curr_xor.hash
+    chars.map(&:ord).hash
   end
 end
 
@@ -28,6 +36,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.sort_by(&:hash).hash
   end
 end
--- hash_map_lru/lib/p03_hash_set.rb
+++ hash_map_lru/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_accessor :store, :count
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,18 +7,28 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key)
+    self[key] << key
+    @count += 1
+    resize! if num_buckets < self.count
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self[key].include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    self.store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +36,9 @@ class HashSet
   end
 
   def resize!
+    curr_store = self.store
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) { Array.new }
+    curr_store.flatten.each { |num| self.insert(num) }
   end
 end
