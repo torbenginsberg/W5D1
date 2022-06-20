90a4215eabd60ad997d617fd07b50f804dd38992
Torben Ginsberg <tginsberg8@gmail.com>
2022-06-20 16:39:29 -0400

    linked lists complete

--- hash_map_lru/lib/p04_linked_list.rb
+++ hash_map_lru/lib/p04_linked_list.rb
@@ -16,11 +16,23 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    old_prev = self.prev
+    old_next = self.next
+    old_prev.next = old_next
+    old_next.prev = old_prev
   end
 end
 
 class LinkedList
+  include Enumerable
+
+  attr_reader :head, :tail
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +41,60 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : self.head.next
   end
 
   def last
+    empty? ? nil : self.tail.prev
   end
 
   def empty?
+    head.next == tail
   end
 
   def get(key)
+    self.each { |node| return node.val if node.key == key }
+    nil
   end
 
   def include?(key)
+    any? { |node| node.key == key }
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    tail.prev.next = new_node
+    new_node.prev = tail.prev
+    new_node.next = tail
+    tail.prev = new_node
   end
 
   def update(key, val)
+    self.each do |node|
+      if node.key == key
+        node.val = val
+      end
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        node.remove
+      end
+    end
   end
 
   def each
+    curr_node = head.next
+    until curr_node == tail
+      yield curr_node
+      curr_node = curr_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
