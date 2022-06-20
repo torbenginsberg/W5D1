7eeb77521d93b3b7d5a55e435d50e9cafa8f5f9e
Torben Ginsberg <tginsberg8@gmail.com>
2022-06-20 14:01:15 -0400

    intset

--- hash_map_lru/lib/p01_int_set.rb
+++ hash_map_lru/lib/p01_int_set.rb
@@ -1,19 +1,30 @@
 class MaxIntSet
+  attr_reader :max, :store
+
   def initialize(max)
+    @max = max
+    @store = Array.new(max + 1, false)
   end
 
   def insert(num)
+    raise "Out of bounds" if !is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    raise "Out of bounds" if !is_valid?(num)
+    @store[num] = false
   end
 
   def include?(num)
+    raise "Out of bounds" if !is_valid?(num)
+    @store[num] == true
   end
 
   private
 
   def is_valid?(num)
+    num >= 0 && num <= max
   end
 
   def validate!(num)
@@ -22,23 +33,30 @@ end
 
 
 class IntSet
+  attr_reader :store
+  
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num) 
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    bucket_idx = num % num_buckets
+    store[bucket_idx]
   end
 
   def num_buckets
